3013a5baa8882ff5b450f9b1b1847bcdad594308
Jin Igarashi <igarashi.jin@water-gis.com>
2022-07-17 13:28:46 +0100

    added some contents required

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import Map from '../components/Map.svelte';
+
+	const title = 'Sveltekit Maplibre boilerplate';
 </script>
+
+<sveltekit:head>
+	<title>{title}</title>
+</sveltekit:head>
 
 <div>
 	<Map />
