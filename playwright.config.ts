6879256ce0761a86c0e6556b34590d764cf74c82
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-11 11:24:32 +0100

    fixed test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,12 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		// if kit.paths.base is not set in production, the following settings can be used.
+		// command: 'npm run build && npm run preview',
+		// port: 4173,
+		// if Github page is used as production, use the following settings
+		command: 'npm run dev',
+		port: 5173
 	}
 };
 
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,4 +5,11 @@ test.describe('index page test', () => {
 		await page.goto('/');
 		await expect(page).toHaveTitle('Sveltekit Maplibre boilerplate');
 	});
+
+	test('index page has expected map container', async ({ page }) => {
+		await page.goto('/');
+		const mapContainer = page.locator('.map');
+		await expect(mapContainer).toBeTruthy();
+		await expect(mapContainer).toHaveCount(1);
+	});
 });
