eb50d43b3d0ed7a6e56f703ddba723e5b3d1a952
Jin Igarashi <igarashi.jin@water-gis.com>
2023-03-12 11:48:37 +0000

    changed to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,16 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			// YOUR github repository name
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-maplibre-boilerplate' : ''
-		}
+		adapter: adapter()
 	}
 };
 
