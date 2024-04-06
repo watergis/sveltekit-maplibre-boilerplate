5110d933aee19aaf779f073d6cecbe222395e219
Jin Igarashi <igarashi.jin@water-gis.com>
2024-04-06 21:56:10 +0100

    fix: changed global map store to use context api

--- src/lib/components/Map.test.ts
+++ src/lib/components/Map.test.ts
@@ -1,22 +1,14 @@
-import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
-import type { Map as MaplibreMap } from 'maplibre-gl';
 import Map from './Map.svelte';
-import { map } from '$lib/stores';
 
 describe('Map.svelte test', () => {
-	let _map: MaplibreMap;
-	beforeEach(() => {
-		map.subscribe((value) => (_map = value));
-	});
-
 	afterEach(() => {
 		vi.clearAllMocks();
 	});
 
-	it('shoud save map object in stores', () => {
+	it('shoud create map class', () => {
 		render(Map);
 		expect(screen.getAllByTestId('map')).toBeTruthy();
-		expect(_map).toBeTruthy();
 	});
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import { createMapStore, MAPSTORE_CONTEXT_KEY } from '$lib/stores';
 	import Map from '$lib/components/Map.svelte';
+	import { setContext } from 'svelte';
+
+	const mapStore = createMapStore();
+	setContext(MAPSTORE_CONTEXT_KEY, mapStore);
 
 	const title = 'Sveltekit Maplibre boilerplate';
 </script>
